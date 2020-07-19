8a73ce475a1d8933fa2995a5cc97964c92d9839a
Maciej Bak <wsciekly.maciek@gmail.com>
2020-07-20 01:14:39 +0200

    add first dummy package test

---
+++ tests/unit/Individual.py
@@ -0,0 +1,18 @@
+"""
+##############################################################################
+#
+#   ?
+#
+#   AUTHOR: Maciej_Bak
+#   AFFILIATION: University_of_Basel
+#   AFFILIATION: Swiss_Institute_of_Bioinformatics
+#   CONTACT: wsciekly.maciek@gmail.com
+#   CREATED: 20-07-2020
+#   LICENSE: MIT
+#
+##############################################################################
+"""
+
+import moran_simulator
+
+moran_simulator.hello()
