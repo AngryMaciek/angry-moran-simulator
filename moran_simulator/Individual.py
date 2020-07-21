f86b4a7353b4b7bfc426491739e9d982d324d740
Maciej Bak <wsciekly.maciek@gmail.com>
2020-07-22 00:26:04 +0200

    add properties to class

--- moran_simulator/Individual.py
+++ moran_simulator/Individual.py
@@ -1,7 +1,7 @@
 """
 ##############################################################################
 #
-#   Init file for the package
+#   Implementation of the population individuals
 #
 #   AUTHOR: Maciej_Bak
 #   AFFILIATION: University_of_Basel
@@ -18,7 +18,39 @@ class Individual:
     Abstract representation of an individual in the population
     '''
     def __init__(self, ID, ind_label):
-        self.ID = ID # private, + getter
-        self.label = ind_label # private, + getter
+        self.ID = ID
+        self.label = ind_label
         self.BirthFitness = None
         self.DeathFitness = None
+
+    @property
+    def ID(self):
+        return self._ID
+
+    @ID.setter
+    def ID(self, ID):
+        self._ID = ID
+    
+    @property
+    def label(self):
+        return self._label
+
+    @label.setter
+    def label(self, label):
+        self._label = label
+
+    @property
+    def BirthFitness(self):
+        return self._BirthFitness
+
+    @BirthFitness.setter
+    def BirthFitness(self, BirthFitness):
+        self._BirthFitness = BirthFitness
+
+    @property
+    def DeathFitness(self):
+        return self._DeathFitness
+
+    @DeathFitness.setter
+    def DeathFitness(self, DeathFitness):
+        self._DeathFitness = DeathFitness
--- tests/unit/Individual.py
+++ tests/unit/Individual.py
@@ -1,7 +1,7 @@
 """
 ##############################################################################
 #
-#   ?
+#   Unit tests for the population individuals
 #
 #   AUTHOR: Maciej_Bak
 #   AFFILIATION: University_of_Basel
@@ -20,8 +20,7 @@ class TestClass:
     
     def test_classIndividualInit(self):
         ind1 = ms.Individual(100, "A")
-        assert ind1.BirthFitness is None and ind1.DeathFitness is None
-
-    # test both getters
-    #    assert ind1.ID == 100
-    #    assert ind1.label == "A"
+        assert ind1.ID == 100
+        assert ind1.label == "A"
+        assert ind1.BirthFitness is None
+        assert ind1.DeathFitness is None
