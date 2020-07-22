50a8d6f8a7225dc3d755bacb13ce828ba373e034
Maciej Bak <wsciekly.maciek@gmail.com>
2020-07-22 11:03:29 +0200

    add MoranProcess class and test

---
+++ moran_simulator/MoranProcess.py
@@ -0,0 +1,37 @@
+"""
+##############################################################################
+#
+#   Implementation of the population evolution
+#
+#   AUTHOR: Maciej_Bak
+#   AFFILIATION: University_of_Basel
+#   AFFILIATION: Swiss_Institute_of_Bioinformatics
+#   CONTACT: wsciekly.maciek@gmail.com
+#   CREATED: 20-07-2020
+#   LICENSE: MIT
+#
+##############################################################################
+"""
+
+class MoranProcess:
+    '''
+    General Moran Process with multiple types of individuals
+    '''
+    def __init__(self, size_list, label_list):
+
+        try:
+            assert len(size_list) == len(label_list)
+        except AssertionError as e:
+            e.args += ("Mismatch length of size and label lists")
+            raise
+
+        ID_counter = 0
+        self.population = []
+        for label_index in range(len(label_list)):
+            for i in range(len(size_list[label_index])):
+                self.population.append(
+                    ms.Individual(ID=ID_counter, ind_label=label_list[label_index])
+                )
+                ID_counter += 1
+
+    # a method to count labels, counter
---
+++ tests/unit/MoranProcess.py
@@ -0,0 +1,22 @@
+"""
+##############################################################################
+#
+#   Unit tests for the population evolution
+#
+#   AUTHOR: Maciej_Bak
+#   AFFILIATION: University_of_Basel
+#   AFFILIATION: Swiss_Institute_of_Bioinformatics
+#   CONTACT: wsciekly.maciek@gmail.com
+#   CREATED: 20-07-2020
+#   LICENSE: MIT
+#
+##############################################################################
+"""
+
+# imports
+import moran_simulator as ms
+
+class TestClass:
+    
+    def test_classMoranProcessInit(self):
+        assert True
