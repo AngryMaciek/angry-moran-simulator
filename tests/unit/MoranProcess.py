a8cb7fab975b556b393951752fe3b8e2a10c9d5c
Maciej Bak <wsciekly.maciek@gmail.com>
2020-08-11 22:05:22 +0200

    correct unit test

--- tests/unit/MoranProcess.py
+++ tests/unit/MoranProcess.py
@@ -203,7 +203,7 @@ class TestClass:
         assert round(mp.AvgDeathPayoffDict["a"], 3) == 0.174
         assert round(mp.AvgDeathPayoffDict["b"], 3) == 0.368
         assert mp.curr_size_list == [6, 14]
-        assert simulation.shape == (11, 10)
+        assert simulation.shape == (11, 11)
 
     def test_classMoranProcessWrongInit(self):
         """Test assertion errors in the initializer."""
