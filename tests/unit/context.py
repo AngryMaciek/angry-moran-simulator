4c449700b1c814d0cc8e20e21039b226224dfcbf
Maciej Bak <wsciekly.maciek@gmail.com>
2021-04-01 21:44:53 +0200

    refactor: linebreaks

--- tests/unit/context.py
+++ tests/unit/context.py
@@ -2,15 +2,6 @@ import os
 import sys
 
 # python path modification to resolve the package properly during testing
-sys.path.insert(
-    0,
-    os.path.abspath(
-        os.path.join(
-            os.path.dirname(__file__),
-            "..",
-            ".."
-        )
-    )
-)
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
 import moranpycess
